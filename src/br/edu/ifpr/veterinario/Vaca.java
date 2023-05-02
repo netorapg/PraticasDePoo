dc960a106229a5d5cc330ba8321d0d342d2fddfd
Argustor <renatoplatz11@outlook.com>
2023-05-02 16:47:36 -0300

    update

---
+++ src/br/edu/ifpr/veterinario/Vaca.java
@@ -0,0 +1,9 @@
+package br.edu.ifpr.veterinario;
+
+public class Vaca extends Gato {
+
+    public Vaca(int idade, double peso, String som, String raca, String cor) {
+        super(idade, peso, som, raca, cor);
+    }
+    
+}
--- src/br/edu/ifpr/veterinario/Veterinario.java
+++ src/br/edu/ifpr/veterinario/Veterinario.java
@@ -4,9 +4,10 @@ public class Veterinario {
     public static void main(String[] args) throws Exception {
         Cachorro cachorro = new Cachorro(5, 10, "Au au", "Poodle");
         Gato gato = new Gato(3, 5, "Miau", "Persa", "Branco");
-   
+        Vaca vaca = new Vaca(10, 100, "Muu", "Holandesa", "Preta e branca");
         System.out.println("Cachorro: " + cachorro.getSom());
         System.out.println("Gato: " + gato.getSom());
+        System.out.println("Vaca: " + vaca.getSom());
     }
 }
 
